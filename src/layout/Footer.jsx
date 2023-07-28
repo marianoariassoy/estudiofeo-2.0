5c913bd1461d4ff78d693498356ed5585117d21a
marianoariassoy <hola@marianoarias.soy>
2023-07-28 13:32:42 -0300

    contact

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -8,35 +8,58 @@ const Footer = () => {
   return (
     <>
       {section !== "contact" && (
-        <section className="px-12 py-24 lg:flex justify-between items-end" id="contacto">
+        <section
+          className="px-12 py-24 lg:flex justify-between items-end"
+          id="contacto"
+        >
           <div>
             <div className="text-xl">
               <div className="flex gap-3">
-                <a href="https://wa.me/5493874848331" className="hover:line-through">
+                <a
+                  href="https://wa.me/5493874848331"
+                  className="hover:line-through"
+                >
                   +54 9 387 4848331
                 </a>
                 |
-                <a href="https://wa.me/5493873436896" className="hover:line-through">
+                <a
+                  href="https://wa.me/5493873436896"
+                  className="hover:line-through"
+                >
                   +54 9 387 3436896
                 </a>
               </div>
               <div>
-                <a href="https://goo.gl/maps/6mCdpgkqNceZ1mMWA" className="hover:line-through" target="_blank" rel="noopener noreferrer">
+                <a
+                  href="https://goo.gl/maps/6mCdpgkqNceZ1mMWA"
+                  className="hover:line-through"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Pueyrredón 190 &bull; Salta | Argentina
                 </a>
               </div>
 
               <div>
                 {section === "shop" ? (
-                  <a href="mailto:tienda.feo@gmail.com" className="hover:line-through">
+                  <a
+                    href="mailto:tienda.feo@gmail.com"
+                    className="hover:line-through"
+                  >
                     tienda.feo@gmail.com
                   </a>
                 ) : section === "gallery" ? (
-                  <a href="mailto:galeria.feo@gmail.com" className="hover:line-through">
+                  <a
+                    href="mailto:galeria.feo@gmail.com"
+                    className="hover:line-through"
+                  >
                     galeria.feo@gmail.com
                   </a>
                 ) : (
-                  <a href="mailto:hola@estudiofeo.com" className="hover:line-through">
+                  <a
+                    href="mailto:hola@estudiofeo.com"
+                    className="hover:line-through"
+                  >
                     hola@estudiofeo.com
                   </a>
                 )}
@@ -45,28 +68,58 @@ const Footer = () => {
               <div className="flex gap-3 items-center">
                 <Instagram />
                 {section === "studio" ? (
-                  <a href="https://instagram.com/estudio.feo" target="_blank" rel="noopener noreferrer" className="hover:line-through">
+                  <a
+                    href="https://instagram.com/estudio.feo"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:line-through"
+                  >
                     @estudio.feo
                   </a>
                 ) : section === "shop" ? (
-                  <a href="https://instagram.com/tienda.feo" target="_blank" rel="noopener noreferrer" className="hover:line-through">
+                  <a
+                    href="https://instagram.com/tienda.feo"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:line-through"
+                  >
                     @tienda.feo
                   </a>
                 ) : section === "gallery" ? (
-                  <a href="https://instagram.com/galeria.feo" target="_blank" rel="noopener noreferrer" className="hover:line-through">
+                  <a
+                    href="https://instagram.com/galeria.feo"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:line-through"
+                  >
                     @galeria.feo
                   </a>
                 ) : (
                   <>
-                    <a href="https://instagram.com/estudio.feo" target="_blank" rel="noopener noreferrer" className="hover:line-through">
+                    <a
+                      href="https://instagram.com/estudio.feo"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:line-through"
+                    >
                       @estudio.feo
                     </a>
                     |
-                    <a href="https://instagram.com/tienda.feo" target="_blank" rel="noopener noreferrer" className="hover:line-through">
+                    <a
+                      href="https://instagram.com/tienda.feo"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:line-through"
+                    >
                       @tienda.feo
                     </a>
                     |
-                    <a href="https://instagram.com/galeria.feo" target="_blank" rel="noopener noreferrer" className="hover:line-through">
+                    <a
+                      href="https://instagram.com/galeria.feo"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:line-through"
+                    >
                       @galeria.feo
                     </a>
                   </>
@@ -76,7 +129,12 @@ const Footer = () => {
                 {section !== "shop" && section !== "gallery" && (
                   <>
                     <Behance />
-                    <a href="https://www.behance.net/estudio_feo" target="_blank" rel="noopener noreferrer" className="hover:line-through">
+                    <a
+                      href="https://www.behance.net/estudio_feo"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:line-through"
+                    >
                       behance.net/estudio_feo
                     </a>
                   </>
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import Layout from "../../layout/Layout";
 import { useDataContext } from "../../context/lanContext";
 import { Helmet } from "react-helmet";
-import { Instagram, Behance, Points } from "../../icons/icons";
+import { Instagram, Behance } from "../../icons/icons";
 import { menu } from "../../data/data";
 
 const Contact = () => {
@@ -20,18 +20,31 @@ const Contact = () => {
       </Helmet>
       <section className="px-12 pt-32 pb-10">
         <div className="text-xl mb-12">
-          <h1 className="font-bold text-7xl mb-4">{lan === "es" ? menu[0].title : menu[0].title_eng}</h1>
+          <h1 className="font-bold text-7xl mb-4">
+            {lan === "es" ? menu[0].title : menu[0].title_eng}
+          </h1>
           <div className="flex gap-3">
-            <a href="https://wa.me/5493874848331" className="hover:line-through">
+            <a
+              href="https://wa.me/5493874848331"
+              className="hover:line-through"
+            >
               +54 9 387 4848331
             </a>
             |
-            <a href="https://wa.me/5493873436896" className="hover:line-through">
+            <a
+              href="https://wa.me/5493873436896"
+              className="hover:line-through"
+            >
               +54 9 387 3436896
             </a>
           </div>
           <div>
-            <a href="https://goo.gl/maps/6mCdpgkqNceZ1mMWA" className="hover:line-through" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://goo.gl/maps/6mCdpgkqNceZ1mMWA"
+              className="hover:line-through"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Pueyrredón 190 &bull; Salta | Argentina
             </a>
           </div>
@@ -42,86 +55,126 @@ const Contact = () => {
           </div>
           <div className="flex gap-3 items-center">
             <Instagram />
-            <a href="https://instagram.com/estudio.feo" target="_blank" rel="noopener noreferrer" className="hover:line-through">
+            <a
+              href="https://instagram.com/estudio.feo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:line-through"
+            >
               @estudio.feo
             </a>
           </div>
           <div className="flex gap-3 items-center">
             <Behance />
-            <a href="https://www.behance.net/estudio_feo" target="_blank" rel="noopener noreferrer" className="hover:line-through">
+            <a
+              href="https://www.behance.net/estudio_feo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:line-through"
+            >
               behance.net/estudio_feo
             </a>
           </div>
-          <div className="mb-6 mt-6">
-            <Points />
-          </div>
-          <div className="font-extraitalic">www.estudiofeo.com</div>
         </div>
 
         <div className="text-xl mb-12">
-          <h1 className="font-bold text-7xl mb-4">{lan === "es" ? menu[1].title : menu[1].title_eng}</h1>
+          <h1 className="font-bold text-7xl mb-4">
+            {lan === "es" ? menu[1].title : menu[1].title_eng}
+          </h1>
           <div className="flex gap-3">
-            <a href="https://wa.me/5493874848331" className="hover:line-through">
+            <a
+              href="https://wa.me/5493874848331"
+              className="hover:line-through"
+            >
               +54 9 387 4848331
             </a>
             |
-            <a href="https://wa.me/5493873436896" className="hover:line-through">
+            <a
+              href="https://wa.me/5493873436896"
+              className="hover:line-through"
+            >
               +54 9 387 3436896
             </a>
           </div>
           <div>
-            <a href="https://goo.gl/maps/6mCdpgkqNceZ1mMWA" className="hover:line-through" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://goo.gl/maps/6mCdpgkqNceZ1mMWA"
+              className="hover:line-through"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Pueyrredón 190 &bull; Salta | Argentina
             </a>
           </div>
           <div>
-            <a href="mailto:tienda.feo@gmail.com" className="hover:line-through">
+            <a
+              href="mailto:tienda.feo@gmail.com"
+              className="hover:line-through"
+            >
               tienda.feo@gmail.com
             </a>
           </div>
           <div className="flex gap-3 items-center">
             <Instagram />
-            <a href="https://instagram.com/tienda.feo" target="_blank" rel="noopener noreferrer" className="hover:line-through">
+            <a
+              href="https://instagram.com/tienda.feo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:line-through"
+            >
               @tienda.feo
             </a>
           </div>
-          <div className="mb-6 mt-6">
-            <Points />
-          </div>
-          <div className="font-extraitalic">www.estudiofeo.com</div>
         </div>
 
         <div className="text-xl mb-12">
-          <h1 className="font-bold text-7xl mb-4">{lan === "es" ? menu[2].title : menu[2].title_eng}</h1>
+          <h1 className="font-bold text-7xl mb-4">
+            {lan === "es" ? menu[2].title : menu[2].title_eng}
+          </h1>
           <div className="flex gap-3">
-            <a href="https://wa.me/5493874848331" className="hover:line-through">
+            <a
+              href="https://wa.me/5493874848331"
+              className="hover:line-through"
+            >
               +54 9 387 4848331
             </a>
             |
-            <a href="https://wa.me/5493873436896" className="hover:line-through">
+            <a
+              href="https://wa.me/5493873436896"
+              className="hover:line-through"
+            >
               +54 9 387 3436896
             </a>
           </div>
           <div>
-            <a href="https://goo.gl/maps/6mCdpgkqNceZ1mMWA" className="hover:line-through" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://goo.gl/maps/6mCdpgkqNceZ1mMWA"
+              className="hover:line-through"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Pueyrredón 190 &bull; Salta | Argentina
             </a>
           </div>
           <div>
-            <a href="mailto:galeria.feo@gmail.com" className="hover:line-through">
+            <a
+              href="mailto:galeria.feo@gmail.com"
+              className="hover:line-through"
+            >
               galeria.feo@gmail.com
             </a>
           </div>
           <div className="flex gap-3 items-center">
             <Instagram />
-            <a href="https://instagram.com/galeria.feo" target="_blank" rel="noopener noreferrer" className="hover:line-through">
+            <a
+              href="https://instagram.com/galeria.feo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:line-through"
+            >
               @galeria.feo
             </a>
           </div>
-          <div className="mb-6 mt-6">
-            <Points />
-          </div>
-          <div className="font-extraitalic">www.estudiofeo.com</div>
         </div>
       </section>
     </Layout>
