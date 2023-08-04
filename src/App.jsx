e8e3e4b5b9780347df0ce349ab4b904f7b47e455
marianoariassoy <hola@marianoarias.soy>
2023-08-04 10:56:01 -0300

    add history back

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,67 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import Home from "./pages/home/Home";
-import Studio from "./pages/studio/Studio";
-import Shop from "./pages/shop/Shop";
-import Gallery from "./pages/gallery/Gallery";
-import About from "./pages/about/About";
-import Error from "./pages/error/Error";
-import Post from "./components/Post";
-import Contact from "./pages/contact/Contact";
+import Home from './pages/home/Home'
+import Studio from './pages/studio/Studio'
+import Shop from './pages/shop/Shop'
+import Gallery from './pages/gallery/Gallery'
+import About from './pages/about/About'
+import Error from './pages/error/Error'
+import Post from './components/Post'
+import Contact from './pages/contact/Contact'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/studio" element={<Studio />} />
-          <Route path="/studio/:id" element={<Post section="studio" />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/shop/:id" element={<Post section="shop" />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/gallery/:id" element={<Post section="gallery" />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/about/:id" element={<Post section="about" />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Error />} />
+          <Route
+            path='/'
+            element={<Home />}
+          />
+          <Route
+            path='/studio'
+            element={<Studio />}
+          />
+          <Route
+            path='/studio/:id'
+            element={<Post />}
+          />
+          <Route
+            path='/shop'
+            element={<Shop />}
+          />
+          <Route
+            path='/shop/:id'
+            element={<Post />}
+          />
+          <Route
+            path='/gallery'
+            element={<Gallery />}
+          />
+          <Route
+            path='/gallery/:id'
+            element={<Post />}
+          />
+          <Route
+            path='/about'
+            element={<About />}
+          />
+          <Route
+            path='/about/:id'
+            element={<Post />}
+          />
+          <Route
+            path='/contact'
+            element={<Contact />}
+          />
+          <Route
+            path='*'
+            element={<Error />}
+          />
         </Routes>
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,67 +1,66 @@
-import { useEffect, useState } from "react";
-import { Link, NavLink, useParams } from "react-router-dom";
-import { Helmet } from "react-helmet";
-import { useDataContext } from "../context/lanContext";
-import useFetch from "../hooks/useFetch";
-import Layout from "../layout/Layout";
-import Loader from "./Loader";
-import TextHTML from "../hooks/useHTML";
-import Modal from "./Modal";
-import ImageComponent from "./ImageComponent";
-import { IconBack, IconVideo, IconForward } from "../icons/icons";
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
+import { useDataContext } from '../context/lanContext'
+import useFetch from '../hooks/useFetch'
+import Layout from '../layout/Layout'
+import Loader from './Loader'
+import TextHTML from '../hooks/useHTML'
+import Modal from './Modal'
+import ImageComponent from './ImageComponent'
+import { IconBack, IconVideo, IconForward } from '../icons/icons'
 
-const Post = ({ section }) => {
-  let { id } = useParams();
-  const { lan } = useDataContext();
-  const { data, loading } = useFetch(`/post/${lan}/${id}`);
-  const { data: dataImages, loading: loadingImages } = useFetch(`/imagenes/${id}`);
-  const [currentVideo, setCurrentVideo] = useState(null);
-  let imageIcon;
+const Post = () => {
+  let { id } = useParams()
+  const { lan } = useDataContext()
+  const { data, loading } = useFetch(`/post/${lan}/${id}`)
+  const { data: dataImages, loading: loadingImages } = useFetch(
+    `/imagenes/${id}`,
+  )
+  const [currentVideo, setCurrentVideo] = useState(null)
+  let imageIcon
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [id]);
+    window.scrollTo(0, 0)
+  }, [id])
 
   if (data) {
     switch (data[0].type) {
       case 2:
-        imageIcon = "image-1.png";
-        break;
+        imageIcon = 'image-1.png'
+        break
       case 3:
-        imageIcon = "image-2.png";
-        break;
+        imageIcon = 'image-2.png'
+        break
       case 4:
-        imageIcon = "image-3.png";
-        break;
+        imageIcon = 'image-3.png'
+        break
       case 5:
-        imageIcon = "image-5.png";
-        break;
+        imageIcon = 'image-5.png'
+        break
       case 6:
-        imageIcon = "image-4.png";
-        break;
+        imageIcon = 'image-4.png'
+        break
       case 7:
-        imageIcon = "image-6.png";
-        break;
+        imageIcon = 'image-6.png'
+        break
 
       default:
-        break;
+        break
     }
   }
 
-  let url;
-  if (section === "studio") url = "/studio";
-  else if (section === "shop") url = "/shop";
-  else if (section === "gallery") url = "/gallery";
-  else if (section === "about") url = "/about";
-
   return (
     <Layout>
-      <section className="lg:flex pt-36 px-12">
-        <div className="post-col-left mb-8">
-          <div className="mb-4">
-            <NavLink to={url} className="hover:opacity-60">
+      <section className='lg:flex pt-36 px-12'>
+        <div className='post-col-left mb-8'>
+          <div className='mb-4'>
+            <button
+              className='hover:opacity-60'
+              onClick={() => window.history.back()}
+            >
               <IconBack />
-            </NavLink>
+            </button>
           </div>
 
           {loading ? (
@@ -72,48 +71,78 @@ const Post = ({ section }) => {
                 <title>FEO &bull; {data[0].title}</title>
               </Helmet>
 
-              <div className="grid grid-cols-2 justify-between items-center lg:grid-cols-1 ">
-                <div className="mb-8 hidden lg:block">
-                  {imageIcon && <img src={`/assets/images/${imageIcon}`} className="w-1/2 mb-4" />}
+              <div>
+                <div className='mb-8 hidden lg:block'>
+                  {imageIcon && (
+                    <img
+                      src={`/assets/images/${imageIcon}`}
+                      className='w-1/2 mb-4'
+                    />
+                  )}
 
-                  <h2 className="font-bold text-3xl">
-                    {data[0].section_name} <span className="font-extraitalic">{data[0].section_name_2} </span>
+                  <h2 className='font-bold text-3xl'>
+                    {data[0].section_name}
+                    <span className='font-extraitalic'>
+                      {data[0].section_name_2}
+                    </span>
                   </h2>
                 </div>
 
                 {data[0].video && (
-                  <div className="justify-end flex items-center lg:mb-8 lg:justify-start">
-                    <button className="hover:opacity-60 open-video" onClick={() => setCurrentVideo(data[0].video)}>
+                  <div className='justify-end flex items-center lg:mb-8 lg:justify-start'>
+                    <button
+                      className='hover:opacity-60 open-video'
+                      onClick={() => setCurrentVideo(data[0].video)}
+                    >
                       <IconVideo />
                     </button>
                   </div>
                 )}
               </div>
 
-              <div className="mb-10">
-                {data[0].number && <h2 className="font-bold text-3xl mb-4 text-outline">{data[0].number}</h2>}
-                {data[0].subtitle && <h2 className="text-2xl">{data[0].subtitle}</h2>}
-                <h1 className="text-4xl font-bold leading-10">{data[0].title}</h1>
+              <div className='mb-10 pr-8'>
+                {data[0].number && (
+                  <h2 className='font-bold text-3xl mb-4 text-outline'>
+                    {data[0].number}
+                  </h2>
+                )}
+                {data[0].subtitle && (
+                  <h2 className='text-2xl'>{data[0].subtitle}</h2>
+                )}
+                <h1 className='text-4xl font-bold leading-10'>
+                  {data[0].title}
+                </h1>
               </div>
 
               {data[0].link && (
-                <div className="mb-4">
-                  <a href={data[0].link} target="_blank" rel="noreferrer" className="border border-black rounded-full px-4 py-2 font-bold inline-flex gap-2 items-center hover:bg-black hover:text-white mb-2">
-                    {lan === "es" ? "Ir al link" : "View Link"}
+                <div className='mb-4'>
+                  <a
+                    href={data[0].link}
+                    target='_blank'
+                    rel='noreferrer'
+                    className='border border-black rounded-full px-4 py-2 font-bold inline-flex gap-2 items-center hover:bg-black hover:text-white mb-2'
+                  >
+                    {lan === 'es' ? 'Ir al link' : 'View Link'}
                   </a>
                 </div>
               )}
 
-              <div className="flex flex-col items-start">
+              <div className='flex flex-col items-start'>
                 {data[0].prev && (
-                  <Link to={`/studio/${data[0].prev}`} className="border border-black rounded-full px-4 py-2 font-bold inline-flex gap-2 items-center hover:bg-black hover:text-white mb-4">
+                  <Link
+                    to={`/studio/${data[0].prev}`}
+                    className='border border-black rounded-full px-4 py-2 font-bold inline-flex gap-2 items-center hover:bg-black hover:text-white mb-4'
+                  >
                     <IconBack />
-                    {lan === "es" ? "Anterior" : "Prev"}
+                    {lan === 'es' ? 'Anterior' : 'Prev'}
                   </Link>
                 )}
                 {data[0].next && (
-                  <Link to={`/studio/${data[0].next}`} className="border border-black rounded-full px-4 py-2 font-bold inline-flex gap-2 items-center hover:bg-black hover:text-white mb-2">
-                    {lan === "es" ? "Siguiente" : "Next"}
+                  <Link
+                    to={`/studio/${data[0].next}`}
+                    className='border border-black rounded-full px-4 py-2 font-bold inline-flex gap-2 items-center hover:bg-black hover:text-white mb-2'
+                  >
+                    {lan === 'es' ? 'Siguiente' : 'Next'}
                     <IconForward />
                   </Link>
                 )}
@@ -122,32 +151,51 @@ const Post = ({ section }) => {
           )}
         </div>
 
-        <div className="post-col-right flex flex-wrap">
+        <div className='post-col-right flex flex-wrap'>
           {loadingImages ? (
             <Loader />
           ) : (
             dataImages &&
             dataImages.map((item, index) => {
               return (
-                <div key={index} className={item.square > 0 ? "w-1/2" : "w-full"}>
-                  <div className={item.square > 0 ? "w-full aspect-square object-cover" : "w-full"}>
-                    <ImageComponent src={item.image} alt={data[0].title} />
+                <div
+                  key={index}
+                  className={item.square > 0 ? 'w-1/2' : 'w-full'}
+                >
+                  <div
+                    className={
+                      item.square > 0
+                        ? 'w-full aspect-square object-cover mb-8'
+                        : 'w-full mb-8'
+                    }
+                  >
+                    <ImageComponent
+                      src={item.image}
+                      alt={data[0].title}
+                    />
                   </div>
                   {item.text && (
-                    <div className="pb-8 lg:p-14">
-                      <TextHTML text={lan === "es" ? item.text : item.text_eng} />
+                    <div className='lg:p-14 lg:pt-6'>
+                      <TextHTML
+                        text={lan === 'es' ? item.text : item.text_eng}
+                      />
                     </div>
                   )}
                 </div>
-              );
+              )
             })
           )}
         </div>
       </section>
 
-      {currentVideo && <Modal currentVideo={currentVideo} setCurrentVideo={setCurrentVideo} />}
+      {currentVideo && (
+        <Modal
+          currentVideo={currentVideo}
+          setCurrentVideo={setCurrentVideo}
+        />
+      )}
     </Layout>
-  );
-};
+  )
+}
 
-export default Post;
+export default Post
